7d09f60a4199b65d1790c289be2150c99d0004fb
Evan Engels <evan.engels@gmail.com>
2017-05-16 13:43:02 -0500

    starting fresh

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyABHUS-qgabTotcAVrtQX9UvhxhXOfcG9A';
+
 //create a new component that produces html
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //take component's html and put it in DOM
