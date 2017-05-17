950e2daebe11b2d00c8b26a24fbdbfc0a960acda
Evan Engels <evan.engels@gmail.com>
2017-05-17 08:02:58 -0500

    youtube search response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyABHUS-qgabTotcAVrtQX9UvhxhXOfcG9A';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 //create a new component that produces html
 const App = () => {
