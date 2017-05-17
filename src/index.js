2fee6a7aaf9c5cfb761717c5072d9c96e20f5c38
Evan Engels <evan.engels@gmail.com>
2017-05-17 12:31:18 -0500

    video list items

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'wingsuit'}, (videos) => {
       // this.setState({ videos: videos });
       this.setState({ videos });
     });
